9e5ea202717b725d1860dce0e5c69e7607c51ce5
Pranjali Rajguru <pranjalirajguru2003@gmail.com>
2026-01-03 18:58:49 +0530

    Add files via upload

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", // FastAPI base URL
+  baseURL: "http://127.0.0.1:8000",
 });
+
 
 // Automatically add token if present
 API.interceptors.request.use((config) => {
